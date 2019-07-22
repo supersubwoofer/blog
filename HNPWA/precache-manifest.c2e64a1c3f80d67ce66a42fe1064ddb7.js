2750ea21df11f3f3f4fb63af073e99e8b9d5f273
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2019-07-22 11:47:49 -0700

    add HNPWA page

---
+++ HNPWA/precache-manifest.c2e64a1c3f80d67ce66a42fe1064ddb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319f383da94c7d4a95145372800463ef",
+    "url": "images/touch/android-chrome-192x192.png"
+  },
+  {
+    "revision": "aa352722e7215e38019603255e259ce7",
+    "url": "images/touch/apple-touch-icon.png"
+  },
+  {
+    "revision": "663909b32580d4b25369ac3f8662ffbf",
+    "url": "images/touch/brand.png"
+  },
+  {
+    "revision": "aebed649ccad8372511a6488bd478f0d",
+    "url": "images/touch/favicon.ico"
+  },
+  {
+    "revision": "9fb8030d7c8287d1df47322e52fc7ade",
+    "url": "index.html"
+  },
+  {
+    "revision": "40d8ae6930d0a758e949",
+    "url": "main.6800fbb6332df00b8800.js"
+  },
+  {
+    "revision": "885380f645652ebb39399fb4883f7aad",
+    "url": "manifest.webmanifest"
+  }
+]);
---
+++ HNPWA/worker.js
@@ -0,0 +1,14 @@
+importScripts("precache-manifest.c2e64a1c3f80d67ce66a42fe1064ddb7.js");
+
+self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+  console.log('Yay! Workbox is loaded 🎉');
+
+  // Precache
+  workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+} else {
+  console.log('Boo! Workbox didn\'t load 😬');
+}
+
